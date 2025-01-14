9b5369d0aafcee03d055e8c332b23ee808b6eef2
Ashish Jha <ajlti2019@gmail.com>
2025-01-15 04:48:26 +0530

    python two sum solution

---
+++ python/Questions/two-sum.py
@@ -0,0 +1,19 @@
+'''
+Solution of two sum problem in python
+'''
+
+
+#**************************************************************************
+#* (C) Copyright by Ashish Jha (ash2kcs@gmail.com) and                    *
+#* Outshine Labs. All Rights Reserved.                                    *
+#*                                                                        *
+#* DISCLAIMER: The authors and publisher of this book/code have used their*
+#* best efforts in preparing the book/code. These efforts include the     *
+#* development, research, and testing of the theories and programs        *
+#* to determine their effectiveness. The authors and publisher make       *
+#* no warranty of any kind, expressed or implied, with regard to these    *
+#* programs or to the documentation contained in these books/code. The    *
+#* authors and publisher shall not be liable in any event for incidental  *
+#*  or consequential damages in connection with, or arising out of, the   *
+#* furnishing, performance, or use of these programs.                     *
+#**************************************************************************
