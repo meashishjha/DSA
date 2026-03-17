62d6cab1f3839875f4d107adcd840a18a454a61e
meashishjha <ashishgm98@gmail.com>
2026-03-17 23:54:02 +0530

    Initial commit of DSA course

--- Python/patterns/patterns.py
+++ Python/patterns/patterns.py
@@ -1,3 +1,17 @@
-#######
-# Problem and solutions to all the patters based problems in python
+#############################################################################
+# (C) Copyright 2026 by Ashish Jha and Outshine Labs.                       #
+# All Rights Reserved.  https://wwww.ashishjha.com/                         #
+# Copyright.  https://wwww.outshinelabs.com/                                #
+#                                                                           #
+# DISCLAIMER: The authors and publisher of this book/code have used their   #
+# best efforts in preparing the book/code. These efforts include the        #
+# development, research, and testing of the theories and programs           #
+# to determine their effectiveness. The authors and publisher make          #
+# no warranty of any kind, expressed or implied, with regard to these       #
+# programs or to the documentation contained in these books/codes.          #
+# The authors and publisher shall not be liable in any event for incidental #
+# or consequential damages in connection with, or arising out of, the       #
+# furnishing, performance, or use of these programs.                        #
+########################=======*********=======##############################
 
+#Solving patterns Questions
